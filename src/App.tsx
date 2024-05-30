7f261dbcff997f1953f87077128e8b253415c8c5
Mattia Di Renzone <mattia.direnzone@giuneco.it>
2024-05-30 16:55:53 +0200

    added form in contact page

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,11 @@ import School from "./routes/School";
 import Contacts from "./routes/Contacts";
 import ScrollToTop from "./utils/scrollToTop";
 import { StyledEngineProvider } from "@mui/styled-engine";
-import { createTheme, ThemeProvider } from "@mui/material";
+import {
+  createTheme,
+  outlinedInputClasses,
+  ThemeProvider,
+} from "@mui/material";
 
 const router = createBrowserRouter([
   {
@@ -60,6 +64,63 @@ const router = createBrowserRouter([
 const theme = createTheme({
   typography: {
     fontFamily: "Della Respira, serif",
+  },
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          "--TextField-brandBorderColor": "#b0a300",
+          "--TextField-brandBorderHoverColor": "#b0a300",
+          "--TextField-brandBorderFocusedColor": "#b0a300",
+          "--TextField-requiredAsteriskColor": "#b0a300",
+          "& label.Mui-focused": {
+            color: "var(--TextField-brandBorderFocusedColor)",
+          },
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          [`&:hover .${outlinedInputClasses.notchedOutline}`]: {
+            borderColor: "var(--TextField-brandBorderHoverColor)",
+          },
+          [`&.Mui-focused .${outlinedInputClasses.notchedOutline}`]: {
+            borderColor: "var(--TextField-brandBorderFocusedColor)",
+          },
+        },
+      },
+    },
+    MuiFilledInput: {
+      styleOverrides: {
+        root: {
+          "&::before, &::after": {
+            borderBottom: "2px solid var(--TextField-brandBorderColor)",
+          },
+          "&:hover:not(.Mui-disabled, .Mui-error):before": {
+            borderBottom: "2px solid var(--TextField-brandBorderHoverColor)",
+          },
+          "&.Mui-focused:after": {
+            borderBottom: "2px solid var(--TextField-brandBorderFocusedColor)",
+          },
+        },
+      },
+    },
+    MuiInput: {
+      styleOverrides: {
+        root: {
+          "&::before": {
+            borderBottom: "2px solid var(--TextField-brandBorderColor)",
+          },
+          "&:hover:not(.Mui-disabled, .Mui-error):before": {
+            borderBottom: "2px solid var(--TextField-brandBorderHoverColor)",
+          },
+          "&.Mui-focused:after": {
+            borderBottom: "2px solid var(--TextField-brandBorderFocusedColor)",
+          },
+        },
+      },
+    },
   },
 });
 
--- src/pages/ContactsPage.tsx
+++ src/pages/ContactsPage.tsx
@@ -1,5 +1,90 @@
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
+
 const ContactsPage = () => {
-  return <div>CONTACTS</div>;
+  const { t } = useTranslation();
+  return (
+    <div>
+      <form action="">
+        <Grid container xs={12} xl={12}>
+          <Grid item xs={12} xl={12} marginTop={"5%"} textAlign={"center"}>
+            <Typography className="title">
+              {t("restiamo-in-contatto")}
+            </Typography>
+            <Typography fontSize={20} mt={"1%"}>
+              Non esitare a scrivermi <br /> Compila il modulo qui sotto con i
+              tuoi recapiti in modo che io possa risponderti il prima possibile.{" "}
+              <br />
+              Grazie!
+            </Typography>
+          </Grid>
+          <form onSubmit={() => alert("CIAOOO")} style={{ marginLeft: "10%" }}>
+            <Grid item xs={12} xl={12} mt={"3%"} mb={"2%"}>
+              <Typography className="small-title" textAlign={"right"}>
+                * {t("campi-obbligatori")}
+              </Typography>
+            </Grid>
+            <Grid item container xs={12} xl={12} spacing={5} mb={"10%"}>
+              <Grid item xl={6} xs={6}>
+                <TextField
+                  className="form-field"
+                  label={t("nome")}
+                  type="text"
+                  required
+                />
+              </Grid>
+              <Grid item xl={6} xs={6}>
+                <TextField
+                  className="form-field"
+                  label={t("cognome")}
+                  type="text"
+                  required
+                />
+              </Grid>
+              <Grid item xl={6} xs={6}>
+                <TextField
+                  className="form-field"
+                  label={t("oggetto")}
+                  type="text"
+                  required
+                />
+              </Grid>
+              <Grid item xl={6} xs={6}>
+                <TextField
+                  className="form-field"
+                  label={"Email"}
+                  type="email"
+                  required
+                />
+              </Grid>
+              <Grid item xl={12} xs={12}>
+                <TextField
+                  className="form-field"
+                  label={t("messaggio")}
+                  type="text"
+                  multiline
+                  minRows={6}
+                  required
+                />
+              </Grid>
+              <Button
+                variant="contained"
+                type="submit"
+                style={{
+                  margin: "5% auto",
+                  marginTop: "5%",
+                  width: 200,
+                  backgroundColor: "#b0a300",
+                }}
+              >
+                {t("invia")}
+              </Button>
+            </Grid>
+          </form>
+        </Grid>
+      </form>
+    </div>
+  );
 };
 
 export default ContactsPage;
