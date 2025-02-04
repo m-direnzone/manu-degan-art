2d7f0c7c9b1486aeb7bac7ec484022f3ab19da5d
Mattia Di Renzone <mattia.direnzone@giuneco.it>
2025-02-04 14:42:52 +0100

    added privacy and cookie policy from Iubenda

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,6 +10,12 @@ const Footer = () => {
     if (window.innerWidth <= 1024) {
       setIsMobileDevice(true);
     }
+
+    // Carica lo script di Iubenda al caricamento del componente
+    const script = document.createElement("script");
+    script.src = "https://cdn.iubenda.com/iubenda.js";
+    script.async = true;
+    document.body.appendChild(script);
   }, []);
   return (
     <div className="footer">
@@ -30,20 +36,59 @@ const Footer = () => {
             <Typography px={5}>
               Ⓒ 2024 - Emanuela Degan - Italian Painter - All right reserved
             </Typography>
-            <Typography>Privacy & cookie policy</Typography>
-            <Typography>Designed and Created by Mattia Di Renzone</Typography>
+            <Typography>
+              <a
+                href="https://www.iubenda.com/privacy-policy/52099859"
+                className="iubenda-white iubenda-noiframe iubenda-embed"
+                title="Privacy Policy"
+              >
+                Privacy Policy
+              </a>{"  "}
+              |{"  "}
+              <a
+                href="https://www.iubenda.com/privacy-policy/52099859/cookie-policy"
+                className="iubenda-white iubenda-noiframe iubenda-embed"
+                title="Cookie Policy"
+              >
+                Cookie Policy
+              </a>
+            </Typography>
+            <Typography>Designed and Created by {" "} 
+              <a href="https://www.linkedin.com/in/mattia-di-renzone-07b75b226/" target="_blank" style={{color: "white"}}>
+              Mattia Di Renzone
+              </a>
+            </Typography>
           </div>
         </>
       ) : (
         <div className="footer-content-container">
           <Typography marginTop={3} marginLeft={10}>
-            Ⓒ 2024 - Emanuela Degan - Italian Painter - All right reserved
+            Ⓒ 2025 - Emanuela Degan - Italian Painter - All right reserved
           </Typography>
           <Typography marginTop={3}>|</Typography>
-          <Typography marginTop={3}>Privacy & cookie policy</Typography>
+          <Typography marginTop={3}>
+            <a
+              href="https://www.iubenda.com/privacy-policy/52099859"
+              className="iubenda-white iubenda-noiframe iubenda-embed"
+              title="Privacy Policy"
+            >
+              Privacy Policy
+            </a>{"  "}
+            |{"  "}
+            <a
+              href="https://www.iubenda.com/privacy-policy/52099859/cookie-policy"
+              className="iubenda-white iubenda-noiframe iubenda-embed"
+              title="Cookie Policy"
+            >
+              Cookie Policy
+            </a>
+          </Typography>
           <Typography marginTop={3}>|</Typography>
           <Typography marginTop={3}>
-            Designed and Created by Mattia Di Renzone
+            Designed and Created by {" "} 
+            <a href="https://www.linkedin.com/in/mattia-di-renzone-07b75b226/" target="_blank" style={{color: "white"}}>
+            Mattia Di Renzone
+            </a>
           </Typography>
           <div className="footer-icon-container">
             <Link
